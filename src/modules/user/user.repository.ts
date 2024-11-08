f8f02a82f70553e9d546221563dbcc4ac5570529
Giordano Cassini <giordano.cassini@gmail.com>
2024-11-08 15:20:08 -0300

    feat: set lacking crud methods

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,4 +1,4 @@
-import { Collection, Db } from 'mongodb';
+import { Collection, Db, ObjectId } from 'mongodb';
 import { User } from './user.entity';
 import { Injectable } from '@nestjs/common';
 
@@ -10,18 +10,53 @@ export class UserRepository {
     this.collection = db.collection('users');
   }
 
-  async createUser({ name, email, cpf, password, userType }: User) {
+  async createUser(user: User): Promise<ObjectId> {
     try {
-      const result = await this.collection.insertOne({
-        name,
-        email,
-        cpf,
-        password,
-        userType,
-      });
+      const result = await this.collection.insertOne(user);
       return result.insertedId;
     } catch (error) {
-      throw new Error('Error creating user: ' + error);
+      throw new Error(`Error creating user in the database: ${error.message}`);
+    }
+  }
+
+  async getUserById(id: string): Promise<User | null> {
+    try {
+      return await this.collection.findOne({ _id: new ObjectId(id) });
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async updateUser(id: string, updateData: Partial<User>): Promise<boolean> {
+    try {
+      const result = await this.collection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData },
+      );
+      return result.matchedCount > 0;
+    } catch (error) {
+      throw new Error(`Error updating user in the database: ${error.message}`);
+    }
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    try {
+      const result = await this.collection.deleteOne({ _id: new ObjectId(id) });
+      return result.deletedCount > 0;
+    } catch (error) {
+      throw new Error(
+        `Error deleting user from the database: ${error.message}`,
+      );
+    }
+  }
+
+  async findAllUsers(): Promise<User[]> {
+    try {
+      return await this.collection.find().toArray();
+    } catch (error) {
+      throw new Error(
+        `Error retrieving users from the database: ${error.message}`,
+      );
     }
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,12 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserRepository } from './user.repository';
-import { User } from './user.entity';
+import { UpdateUserDto, User } from './user.entity';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class UserService {
@@ -10,7 +16,65 @@ export class UserService {
     try {
       return await this.userRepository.createUser(user);
     } catch (error) {
-      throw new HttpException(`Unable to create user: ${error}`, 500);
+      throw new HttpException(`Unable to create user: ${error.message}`, 500);
+    }
+  }
+
+  async getUserById(id: string): Promise<User> {
+    if (!ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid ID format: ${id}`);
+    }
+    try {
+      const user = await this.userRepository.getUserById(id);
+      if (!user) throw new NotFoundException(`User with ID ${id} not found.`);
+      return user;
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+
+  async updateUser(id: string, updateData: UpdateUserDto): Promise<void> {
+    if (!ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid ID format: ${id}`);
+    }
+    try {
+      const updated = await this.userRepository.updateUser(id, updateData);
+      if (!updated) {
+        throw new NotFoundException(`User with ID ${id} not found`);
+      }
+    } catch (error) {
+      throw new HttpException(
+        `Unable to update user: ${error.message}`,
+        error.status,
+      );
+    }
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    if (!ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid ID format: ${id}`);
+    }
+    try {
+      const deleted = await this.userRepository.deleteUser(id);
+      if (!deleted) {
+        throw new NotFoundException(`User with ID ${id} not found`);
+      }
+    } catch (error) {
+      throw new HttpException(
+        `Unable to delete user: ${error.message}`,
+        error.status,
+      );
+    }
+  }
+
+  async findAllUsers(): Promise<User[]> {
+    try {
+      return await this.userRepository.findAllUsers();
+    } catch (error) {
+      throw new HttpException(
+        `Unable to retrieve users: ${error.message}`,
+        500,
+      );
     }
   }
 }
