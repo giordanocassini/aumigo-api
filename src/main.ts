a9c1d4cbcd8467e39a95f9fba56b3242f83f00ab
Giordano Cassini <giordano.cassini@gmail.com>
2024-11-11 17:03:55 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // Allow all origins
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Strips out any properties that aren't in the DTO
